8ffdb878cb57beb1ada460e06347a7a28c67c213
alu0101056944 <alu0101056944@ull.edu.es>
2023-11-22 16:53:13 +0000

    Incorrect data fetching from mongodb. WIP.

--- web-server/app.js
+++ web-server/app.js
@@ -5,12 +5,34 @@ import express from 'express';
 import {fileURLToPath} from 'url';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import nocache from 'nocache';
 
 mongoose.connect('mongodb://localhost:27017/maindatabase',
     { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+// cli:
+// mongoimport --db maindatabase  --collection foo  --file assets/goods.json
+
 const goodsModel =
-    mongoose.model('goods', new mongoose.Schema({ bienes: String }));
+    mongoose.model('goods', new mongoose.Schema(
+      {
+        bienes: [
+            {
+              nombre: String,
+              antecedentes: String,
+              tipo: {
+                arquitectura: String,
+                época: String
+              },
+              img: String,
+              localizacion: {
+                lat: String,
+                long: String
+              }
+            }
+          ]
+      }));
 
 // Create and serve http server
 function main() {
@@ -18,15 +40,17 @@ function main() {
   const PORT = 8080;
   app.set('port', PORT);
   app.use(cors());
+  app.use(nocache());
 
-  app.listen(PORT, '127.0.0.1', () => {
+  app.listen(PORT, 'localhost', () => {
         console.log('Server listening at 0.0.0.0:' + PORT);
       });
 
   app.get('/', async (request, response) => {
         try {
           const RESPONSE_CONTENT = await goodsModel.find();
-          response.json(RESPONSE_CONTENT);
+          response.send(JSON.stringify(RESPONSE_CONTENT, null, 2));
+          console.log('able to fetch from db');
         } catch (error) {
           console.error('Could not fetch information from mongo database: ' + error);
           console.error('Did not start express web server.');
